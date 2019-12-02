b29eacc7fb2de65901f1000ebdda2f5e8966ea75
Oliver Schulz <oschulz@mpp.mpg.de>
2019-12-02 10:03:04 -0800

    STASH

--- src/ArraysOfArrays.jl
+++ src/ArraysOfArrays.jl
@@ -4,6 +4,7 @@ __precompile__(true)
 
 module ArraysOfArrays
 
+#=
 using Requires
 using Statistics
 using UnsafeArrays
@@ -18,6 +19,6 @@ function __init__()
     @require StaticArrays = "90137ffa-7385-5640-81b9-e52037218182" include("staticarrays_support.jl")
     @require StatsBase = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91" include("statsbase_support.jl")
 end
-
+=#
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@
 using Test
 
 Test.@testset "Package ArraysOfArrays" begin
+    #=
     include("functions.jl")
     include("array_of_similar_arrays.jl")
     include("vector_of_arrays.jl")
+    =#
 end
