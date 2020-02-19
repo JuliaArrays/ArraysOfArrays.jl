40a9f95187ea0aede1f1e23003633656595bc8ed
Scott Hayashi <Scott.Hayashi@tum.de>
2020-02-20 00:22:45 +0100

    Added tests for StatsBase support

--- test/vector_of_arrays.jl
+++ test/vector_of_arrays.jl
@@ -1,6 +1,8 @@
 # This file is a part of ArraysOfArrays.jl, licensed under the MIT License (MIT).
 
 using ArraysOfArrays
+using StatsBase
+using Statistics
 using Test
 
 using UnsafeArrays
@@ -153,6 +155,17 @@ using ArraysOfArrays: full_consistency_checks, append_elemptr!, element_ptr
 
         @test typeof(@inferred empty(B)) == typeof(B)
         @test empty(A) == empty(B)
+    end
+
+    @testset "StatsBase support" begin
+        r = rand(1,100)
+        V = VectorOfSimilarVectors{Float64}(r)
+        w = FrequencyWeights(rand(100))
+        @test isapprox(@inferred(sum(V, w))[1], sum(r, w))
+        @test isapprox(@inferred(mean(V,w))[1], mean(r, w))
+        @test isapprox(@inferred(var(V,w, corrected=true))[1], var(r, w, corrected=true))
+        @test isapprox(@inferred(cov(V, w))[1], var(r, w, corrected=true))
+        @test isapprox(@inferred(cor(V,w))[1], 1.0)
     end
 
     @testset "examples" begin
