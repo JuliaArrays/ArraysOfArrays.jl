36aba3b927b0d6991325521744c43fb347658c7a
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-11 18:05:34 +0100

    Update other stats as well

--- src/statsbase_support.jl
+++ src/statsbase_support.jl
@@ -7,14 +7,14 @@ Base.sum(X::AbstractVectorOfSimilarArrays{T,M}, w::StatsBase.AbstractWeights) wh
 Statistics.mean(X::AbstractVectorOfSimilarArrays{T,M}, w::StatsBase.AbstractWeights) where {T,M} =
     mean(flatview(X), w, dims = M + 1)[_ncolons(Val{M}())...]
 
-Statistics.var(X::AbstractVectorOfSimilarArrays{T,M}, w::StatsBase.AbstractWeights; corrected::Bool = true) where {T,M} =
-    var(flatview(X), w, M + 1; corrected = corrected)[_ncolons(Val{M}())...]
+Statistics.var(X::AbstractVectorOfSimilarArrays{T,M}, w::StatsBase.AbstractWeights; mean = nothing, corrected::Bool = true) where {T,M} =
+    var(flatview(X), w, M + 1; mean = mean, corrected = corrected)[_ncolons(Val{M}())...]
 
 Statistics.std(X::AbstractVectorOfSimilarArrays{T,M}, w::StatsBase.AbstractWeights; mean = nothing, corrected::Bool = true) where {T,M} =
     std(flatview(X), w, M + 1; mean = mean, corrected = corrected)[_ncolons(Val{M}())...]
 
-Statistics.cov(X::AbstractVectorOfSimilarVectors, w::StatsBase.AbstractWeights; corrected::Bool = true) =
-    cov(flatview(X), w, 2; corrected = corrected)
+Statistics.cov(X::AbstractVectorOfSimilarVectors, w::StatsBase.AbstractWeights; mean = nothing, corrected::Bool = true) =
+    cov(flatview(X), w, 2; mean = mean, corrected = corrected)
 
 Statistics.cor(X::AbstractVectorOfSimilarVectors, w::StatsBase.AbstractWeights) =
     cor(flatview(X), w, 2)
