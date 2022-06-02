6d67dde8378b9befd972acf0bcfe85b78f3b3b43
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-02 22:57:31 +0200

    Document module ArraysOfArrays

--- src/ArraysOfArrays.jl
+++ src/ArraysOfArrays.jl
@@ -2,6 +2,15 @@
 
 __precompile__(true)
 
+"""
+    module ArraysOfArrays
+
+Efficient storage and handling of nested arrays.
+
+ArraysOfArrays provides two different types of nested arrays:
+[`ArrayOfSimilarArrays`](@ref section_ArrayOfSimilarArrays) and
+[`VectorOfArrays`](@ref section_VectorOfArrays).
+"""
 module ArraysOfArrays
 
 using Adapt
