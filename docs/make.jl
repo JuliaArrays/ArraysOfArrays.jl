773f4661bcdbaffee4d6bc39fa68c34aac2e4681
Oliver Schulz <oschulz@mpp.mpg.de>
2023-12-11 17:25:52 -0800

    Update Documenter to v1

--- docs/make.jl
+++ docs/make.jl
@@ -29,7 +29,7 @@ makedocs(
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
     linkcheck = !("nonstrict" in ARGS),
-    strict = !("nonstrict" in ARGS),
+    warnonly = ("nonstrict" in ARGS),
 )
 
 deploydocs(
