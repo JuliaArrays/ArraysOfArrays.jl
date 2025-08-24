2a263f5222060193fd22519e1ce970b62eafc3f9
Oliver Schulz <oschulz@mpp.mpg.de>
2025-08-24 23:07:28 +0200

    STASH splitview

--- test/base_slices.jl
+++ test/base_slices.jl
@@ -31,10 +31,76 @@ using ArraysOfArrays: getinnerdims, getouterdims
 
     A = Aes2
     A_unsplit_ref = A_orig
+    f = x -> x^2
 
+    @test Array(A) isa Array{<:Any,ndims(A)}
+    A_array = Array(A)
+    @test A == A_array
+    @test isequal(A, A_array)
+
+    @test @infered(getsplitmode(A)) isa AbstractSplitMode
     smode = getsplitmode(A)
-    @test @inferred(joinedview(A)) === A_unsplit_ref
-    A_unsplit = joinedview(A)
-    @test typeof(splitview(A_unsplit_ref, smode)) == typeof(A)
-    @test splitview(A_unsplit_ref, smode) == A
+    if A isa AbstractSlices
+        let M = ndims(eltype(A)), N = ndims(A)
+            @test smode isa AbstractSlicingMode{M,N}
+        end
+    end
+
+    @test @inferred(eltype(A)) isa AbstractArray
+    T_elem = eltype(A)
+    if !isemtpy(A)
+        @test @inferred(A[begin]) isa AbstractArray
+        A_1 == A[begin]
+        @test typeof(@inferred(A[begin])) == T_elem
+        @test @inferred(innersize(A)) == size(A_1)
+    end
+
+    @inferred(innermap(f, A)) == innermap(f, Array(A))
+    @inferred(deepmap(f, A)) == deepmap(f, Array(A))
+
+    _smode_M(::AbstractSlicingMode{M,N}) where {M,N} = M
+    _smode_N(::AbstractSlicingMode{M,N}) where {M,N} = N
+
+    if smodes isa AbstractSlicingMode
+        M, N = _smode_M(smode), _smode_N(smode)
+        A_array_stacked = stack(A_array)
+        @test M == ndims(eltype(A))
+        @test N == ndims(A)
+
+        @test stack(A) == A_array_stacked
+
+        if is_memordered_splitmode(smode)
+            if A isa Slices
+                # stack(A) never returns parent for Slices even if possible:
+                @test @inferred(stack(A)) == A_unsplit_ref
+            else
+                @test @inferred(stack(A)) === A_unsplit_ref
+            end
+            @test @inferred(stacked(A)) === A_unsplit_ref
+            @test @inferred(flatview(A)) === A_unsplit_ref
+        else
+            @test Array(@inferred(stack(A))) == A_array_stacked
+            @test Array(@inferred(stacked(A))) == A_array_stacked
+            @test_throws ArgumentError flatview(A)
+        end
+
+        let dimstpl = ntuple(identity, Val(ndims(A_unsplit_ref)))
+            @test @infered(getinnerdims(dimstpl, smode)) isa NTuple{M,Int}
+            @test @infered(getouterdims(dimstpl, smode)) isa NTuple{N,Int}
+            innerdims = getinnerdims(dimstpl, smode)
+            outerdims = getouterdims(dimstpl, smode)
+            @test Array(permutedims(A_unsplit_ref, (outerdims..., innerdims...))) == A_array_stacked
+        end
+    end
+
+    if smode isa UnknownSplitMode
+        @test_throws ArgumentError joinedview(A)
+        @test_throws ArgumentError flatview(A)
+        @test_throws ArgumentError splitview(A_unsplit_ref, smode)
+    else
+        @test @inferred(joinedview(A)) === A_unsplit_ref
+        A_unsplit = joinedview(A)
+        @test typeof(splitview(A_unsplit_ref, smode)) == typeof(A)
+        @test splitview(A_unsplit_ref, smode) == A
+    end
 end
